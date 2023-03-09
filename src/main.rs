f270f43c38f3ca304d0cc57bd91ddb4fc03a86ed
Trubiso <trubiso@users.noreply.github.com>
2023-03-09 22:30:50 +0100

    better main function that takes in file(s)

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ pub mod parser;
 pub mod resolve;
 pub mod span;
 
-fn emit_errors(files: &SimpleFiles<&str, &String>, diagnostics: Vec<Diagnostic<usize>>) {
+fn emit_errors(files: &SimpleFiles<String, String>, diagnostics: Vec<Diagnostic<usize>>) {
 	let writer = StandardStream::stderr(ColorChoice::Always);
 	let config = term::Config::default();
 	let amount = diagnostics.len();
@@ -31,46 +31,52 @@ fn emit_errors(files: &SimpleFiles<&str, &String>, diagnostics: Vec<Diagnostic<u
 
 fn main() {
 	let mut files = SimpleFiles::new();
-	let code = fs::read_to_string("code").unwrap();
-	let file_id = files.add("code", &code);
+	let mut args = std::env::args();
+	args.next();
+	while let Some(arg) = args.next() {
+		let code = fs::read_to_string(arg.clone()).unwrap();
+		let file_id = files.add(arg.clone(), code);
 
-	let now = std::time::Instant::now();
-	eprint!("Lexing...");
+		let now = std::time::Instant::now();
+		eprint!("Lexing...");
 
-	let lexed = match lexer::lex(&code, file_id) {
-		Ok(x) => x,
-		Err(x) => return emit_errors(&files, x),
-	};
+		let lexed = match lexer::lex(files.get(file_id).unwrap().source(), file_id) {
+			Ok(x) => x,
+			Err(x) => return emit_errors(&files, x),
+		};
 
-	eprintln!("\rLexed in {}ms", now.elapsed().as_millis());
-	let now = std::time::Instant::now();
-	eprint!("Parsing...");
+		eprintln!("\rLexed in {}ms", now.elapsed().as_millis());
+		let now = std::time::Instant::now();
+		eprint!("Parsing...");
 
-	let lexed_iter: CodeStream = Stream::from_iter(
-		Span::new(file_id, code.len()..code.len()),
-		lexed.into_iter(),
-	);
-	let parsed = match parser::parse(lexed_iter) {
-		Ok(x) => x,
-		Err(x) => return emit_errors(&files, x),
-	};
+		let code_len = files.get(file_id).unwrap().source().len();
 
-	eprintln!("\rParsed in {}ms", now.elapsed().as_millis());
-	let now = std::time::Instant::now();
-	eprint!("Resolving...");
+		let lexed_iter: CodeStream = Stream::from_iter(
+			Span::new(file_id, code_len..code_len),
+			lexed.into_iter(),
+		);
+		let parsed = match parser::parse(lexed_iter) {
+			Ok(x) => x,
+			Err(x) => return emit_errors(&files, x),
+		};
 
-	let resolved = match resolve::resolve(parsed, resolve::Context::TopLevel, None, None) {
-		Ok((x, _)) => x,
-		Err(x) => return emit_errors(&files, x),
-	};
+		eprintln!("\rParsed in {}ms", now.elapsed().as_millis());
+		let now = std::time::Instant::now();
+		eprint!("Resolving...");
 
-	eprintln!("\rResolved in {}ms", now.elapsed().as_millis());
-	let now = std::time::Instant::now();
-	eprint!("Codegenning...");
+		let resolved = match resolve::resolve(parsed, resolve::Context::TopLevel, None, None) {
+			Ok((x, _)) => x,
+			Err(x) => return emit_errors(&files, x),
+		};
 
-	let code = codegen::codegen(resolved);
+		eprintln!("\rResolved in {}ms", now.elapsed().as_millis());
+		let now = std::time::Instant::now();
+		eprint!("Codegenning...");
 
-	eprintln!("\rCodegenned in {}ms", now.elapsed().as_millis());
+		let code = codegen::codegen(resolved);
 
-	println!("{code}");
+		eprintln!("\rCodegenned in {}ms", now.elapsed().as_millis());
+
+		std::fs::write(format!("{arg}.cpp"), code);
+	}
 }
