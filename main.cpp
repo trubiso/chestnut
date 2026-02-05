f853c805b229f96a6c69bd6a3c1f494320469468
trubiso <33161454+trubiso@users.noreply.github.com>
2026-02-05 16:44:39 +0100

    feat: abort codegen if there are any errors

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <string>
 
+size_t errors = 0, warnings = 0;
+
 std::optional<std::vector<std::string>> get_sources(std::vector<std::string> const& files_to_parse) {
 	std::vector<std::string> sources {};
 	for (std::string const& name : files_to_parse) {
@@ -23,22 +25,22 @@ std::optional<std::vector<std::string>> get_sources(std::vector<std::string> con
 	return sources;
 }
 
-std::tuple<size_t, std::vector<std::tuple<Stream<Token>, FileContext>>>
+std::vector<std::tuple<Stream<Token>, FileContext>>
 lex_files(std::vector<std::string> const& sources, std::vector<std::string> const& names) {
 	std::vector<std::tuple<Stream<Token>, FileContext>> outputs {};
 	assert(sources.size() == names.size());
-	size_t diagnostics = 0;
 	for (size_t i = 0; i < sources.size(); ++i) {
 		FileContext   context {names[i], (FileContext::ID) i, {0}, sources[i]};
 		Lexer         lexer(context, sources[i]);
 		Stream<Token> tokens {std::move(lexer.collect_all())};
 		outputs.push_back({std::move(tokens), lexer.context});
 		for (auto const& diagnostic : lexer.diagnostics()) {
-			diagnostics++;
 			diagnostic.print();
+			if (diagnostic.severity == Diagnostic::Severity::Error) errors++;
+			else warnings++;
 		}
 	}
-	return {diagnostics, outputs};
+	return outputs;
 }
 
 std::vector<Resolver::ParsedFile> parse_files(std::vector<std::tuple<Stream<Token>, FileContext>>&& lexed_files) {
@@ -46,7 +48,12 @@ std::vector<Resolver::ParsedFile> parse_files(std::vector<std::tuple<Stream<Toke
 	for (auto&& [tokens, context] : std::move(lexed_files)) {
 		AST::Parser parser {context, std::move(tokens)};
 		AST::Module parsed = parser.parse_all(context.name);
-		for (auto const& diagnostic : parser.diagnostics()) { diagnostic.print(); }
+		for (auto const& diagnostic : parser.diagnostics()) {
+			diagnostic.print();
+
+			if (diagnostic.severity == Diagnostic::Severity::Error) errors++;
+			else warnings++;
+		}
 
 		parsed_files.push_back(
 			Resolver::ParsedFile {
@@ -71,20 +78,39 @@ int main(void) {
 	if (!maybe_sources.has_value()) return errno;
 	std::vector<std::string> sources = std::move(maybe_sources.value());
 
-	auto [diagnostic_count, lexed_files] = lex_files(sources, files_to_parse);
+	auto lexed_files = lex_files(sources, files_to_parse);
 
 	std::vector<Resolver::ParsedFile> parsed_files = parse_files(std::move(lexed_files));
 
-	Resolver resolver {std::move(parsed_files)};
+	Resolver                resolver {std::move(parsed_files)};
 	std::vector<IR::Module> modules = resolver.resolve();
 
-	// for (auto const& module : modules) { std::cout << module << std::endl; }
-
 	for (auto const& file : resolver.parsed_files)
-		for (auto const& diagnostic : file.diagnostics) diagnostic.print();
+		for (auto const& diagnostic : file.diagnostics) {
+			diagnostic.print();
+			if (diagnostic.severity == Diagnostic::Severity::Error) errors++;
+			else warnings++;
+		}
 
-	// resolver.dump();
-	
+	// if there are errors, do not proceed to codegen
+	if (errors > 0) {
+		std::cerr << "aborting due to " << errors << " error";
+		if (errors != 1) std::cerr << 's';
+		if (warnings > 0) {
+			std::cerr << " and " << warnings << " warning";
+			if (warnings != 1) std::cerr << 's';
+		}
+		std::cerr << std::endl;
+		return 1;
+	}
+
+	// if there aren't any errors but there are warnings, tell the user the count
+	if (warnings > 0) {
+		std::cerr << "produced " << warnings << " warning";
+		if (warnings != 1) std::cerr << 's';
+		std::cerr << std::endl;
+	}
+
 	CodeGenerator generator(resolver.export_symbols());
 	generator.process(modules);
 }
