8d591d3185adc9a70d043f30bdd3e8d723afd56f
trubiso <33161454+trubiso@users.noreply.github.com>
2026-03-05 11:47:06 +0100

    fix(resolver/types): unify manually for function overloads
    this finally makes the arguments unify amongst themselves in functions
    like func f<T>(a: T, b: T)! it also makes the generics unify correctly
    and push the proper bounds :)

--- src/resolver_types.cpp
+++ src/resolver_types.cpp
@@ -1571,7 +1571,43 @@ void Resolver::constrain_candidate(UndecidedOverload& undecided_overload) {
 		= register_type(std::move(candidate.call_type), undecided_overload.span, undecided_overload.file_id);
 
 	// now we must constrain the candidate
-	unify(call_id, candidate.function, undecided_overload.file_id);
+	FileContext::ID file_id = get_type_file_id(call_id);
+
+	assert(type_pool_.at(call_id).is_function() && type_pool_.at(candidate.function).is_function());
+
+	TypeInfo::Function &call      = type_pool_.at(call_id).get_function(),
+			   &prototype = type_pool_.at(candidate.function).get_function();
+
+	assert(call.arguments.size() == prototype.arguments.size());
+	assert(call.generics.size() == prototype.generics.size());
+
+	std::unordered_map<TypeInfo::ID, TypeInfo::ID> generic_map {};
+	for (size_t i = 0; i < call.generics.size(); ++i) {
+		generic_map.insert_or_assign(std::get<1>(prototype.generics.at(i)), std::get<1>(call.generics.at(i)));
+	}
+
+	for (size_t i = 0; i < call.generics.size(); ++i) {
+		auto& call_generic      = type_pool_.at(std::get<1>(call.generics.at(i))).get_generic();
+		auto& prototype_generic = type_pool_.at(std::get<1>(prototype.generics.at(i))).get_generic();
+
+		for (auto const& constraint : prototype_generic.declared_constraints) {
+			call_generic.imposed_constraints.push_back(instantiate_constraint(constraint, generic_map));
+		}
+	}
+
+	// we create a new prototype with the generics all replaced
+	auto const& new_prototype = type_pool_.at(instantiate_type(candidate.function, generic_map)).get_function();
+
+	// unify args
+	for (size_t i = 0; i < call.arguments.size(); ++i) {
+		TypeInfo::ID call_arg_type      = std::get<1>(call.arguments.at(i));
+		TypeInfo::ID prototype_arg_type = std::get<1>(new_prototype.arguments.at(i));
+
+		unify(call_arg_type, prototype_arg_type, file_id);
+	}
+
+	// unify return type
+	unify(call.return_, new_prototype.return_, file_id);
 
 	// if we're calling an identifier, let's finish resolving it
 	if (undecided_overload.identifier.has_value()) {
@@ -2736,6 +2772,7 @@ Resolver::infer(AST::Expression::FunctionCall& function_call, Span span, FileCon
 		assert(function_generics.size() == provided_generics);
 		std::vector<std::tuple<std::optional<std::string>, TypeInfo::ID>> generics {};
 		generics.reserve(provided_generics);
+		// FIXME: this breaks with built-in functions!
 		auto const& original_function
 			= *std::get<AST::Function*>(symbol_pool_.at(type_symbol_mapping_.at(callable_id).value()).item);
 		for (size_t i = 0; i < provided_generics; ++i) {
@@ -3602,6 +3639,7 @@ void Resolver::decide_remaining_types() {
 
 void Resolver::infer_types() {
 	for (ParsedFile& file : parsed_files) { infer(file.module, file.file_id); }
+	// FIXME: we don't know whether this candidate is suitable, we should filter first!
 	constrain_known_named_type_generics();
 	decide_remaining_types();
 }
